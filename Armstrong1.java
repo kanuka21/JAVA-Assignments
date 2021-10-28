99a9c19d091741345d91e52bb56214f0f0e7d780
kanuka goel <65423384+kanuka21@users.noreply.github.com>
2021-10-28 21:54:57 +0530

    Add files via upload

---
+++ Armstrong1.java
@@ -0,0 +1,28 @@
+// find out all Armstrong numbers falling in the range of 100-999
+
+public class Armstrong1
+
+  {
+    public static void main(String args[])
+   {
+
+     for(int k=100; k<=999; k++)
+    {
+         int n=k;
+         int d=0;
+         int s =0;
+
+                while(n>0)
+        {
+             d=n%10;
+             s=s+(d*d*d);
+             n=n/10;
+
+        }
+     if(k==s)
+        {
+       System.out.println(k+ "Armstrong");
+         }
+      }
+ }
+}
